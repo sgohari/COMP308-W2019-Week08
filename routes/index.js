663eb26251d1b74a95cbfc561add0037529494bf
sgohary <nasir_2005@msn.com>
2019-02-05 16:22:29 -0500

    updated package and express js is added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nasir' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;

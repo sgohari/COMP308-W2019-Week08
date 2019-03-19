3cc4c30cc2961bfea6f4cfef50ed1727303a498f
sgohary <syednacer@gmail.com>
2019-03-19 17:01:40 -0400

    Enabled user authorization

--- server/routes/favourit.js
+++ server/routes/favourit.js
@@ -3,23 +3,36 @@ let router = express.Router();
 //for using mongo
 let mongoose = require('mongoose');
 
+//reference to passports
+let passport = require('passport');
+
+function requiredAuth(req, res, next){
+    //checking if the user is logged in
+    //console.log(req.user.username);
+    if(!req.isAuthenticated()){
+      return res.redirect('/login');
+    }
+ 
+    next();
+  }
+
 
 let favouriteController=require('../controllers/favourites');
-router.get('/', favouriteController.displayFavouriteList);
+router.get('/',requiredAuth, favouriteController.displayFavouriteList);
 
 // GET route for the add page
-router.get('/add', favouriteController.displayAddFavourits);
+router.get('/add',requiredAuth, favouriteController.displayAddFavourits);
 
 /*Post Route processing the add*/
 router.post('/add', favouriteController.processAddFavouritList);
 
 /*GET request display EDIT page */
-router.get('/edit/:id', favouriteController.displayEditPage);
+router.get('/edit/:id', requiredAuth, favouriteController.displayEditPage);
 
 /* POST request to update the DB with data from Edit page */
-router.post('/edit/:id', favouriteController.proccessEditPage);
+router.post('/edit/:id',requiredAuth, favouriteController.proccessEditPage);
 
 /**GET request to perform the delete action */
-router.get('/delete/:id', favouriteController.performDelete);
+router.get('/delete/:id',requiredAuth, favouriteController.performDelete);
 
 module.exports = router;

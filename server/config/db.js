1c2e0ed4383d6298fb583ad6774083815c14dfdf
sgohary <syednacer@gmail.com>
2019-03-20 12:42:23 -0400

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-     "URI" : "mongodb://localhost/nasir"
+     "URI" : "mongodb://localhost/nasir",
+     "secret" : "someSecrete"
     //"URI":"mongodb://nacer:amin2005@ds018258.mlab.com:18258/nasir"
 }
